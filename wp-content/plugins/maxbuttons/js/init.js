d3be38a577692b0e1cfb04be7a1822ed4a07a2f7
Joseph Chagnon <chagnon.5@osu.edu>
2015-11-07 15:50:33 -0500

    initial plugins

---
+++ wp-content/plugins/maxbuttons/js/init.js
@@ -0,0 +1,11 @@
+ 
+jQuery(document).ready(function($) {	
+ 
+ 	//j$ = $.noConflict();
+  	var max = new maxAdmin(); 
+ 	max.init(); 
+
+	var mc = new maxCollection(); 
+	mc.init();  
+ 	
+}); /* END OF JQUERY */
